b6e1e42f9545d48e3b47367b46d13ee55e1a6e73
infinity-intellect <snavaneetharan@gmail.com>
2020-07-10 20:14:27 +0530

    Called mongo connection function

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
-const express=require('express')
+const express = require('express')
 
-const app=express()
-const port=process.env.PORT || 3000
+const app = express()
+const port = process.env.PORT || 3000
+
+const mongoconnect = require('./src/db/mongoose')
 
 app.use(express.json)
 
-app.listen(port , () => {
-    console.log('Server is running on '+ port)
+app.listen(port, () => {
+    console.log('Server is running on ' + port)
 })
+mongoconnect()
--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,7 +1,19 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://nanda:nanda@resumer.lixlr.mongodb.net/test?authSource=admin&replicaSet=atlas-shng1u-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true/resumer',{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:true
-})
+const mongoconnect = () => {
+    mongoose.connect('mongodb+srv://nanda:nanda@resumer.lixlr.mongodb.net/test?authSource=admin&replicaSet=atlas-shng1u-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true/resumer', {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true
+    }, err => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("Connected to Mongodb atlas")
+        }
+    })
+}
+
+module.exports = mongoconnect
