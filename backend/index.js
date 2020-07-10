5adb5960e6bacb6c15a11f3d21d02b69090db4de
infinity-intellect <snavaneetharan@gmail.com>
2020-07-10 20:25:10 +0530

    Console logged database connection

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
+const mongodb = require('./mongodb')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const mongoconnect = require('./src/db/mongoose')
 
 app.use(express.json)
 
 app.listen(port, () => {
     console.log('Server is running on ' + port)
 })
-mongoconnect()
--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,13 +1,16 @@
-const mongodb= require('mongodb')
-const MongoClient= mongodb.MongoClient
-const ObjectID=mongodb.ObjectID
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 
-const connectionURL='mongodb+srv://nanda:nanda@resumer.lixlr.mongodb.net/test?authSource=admin&replicaSet=atlas-shng1u-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
-const databaseName='resumer'
+const connectionURL = 'mongodb+srv://nanda:nanda@resumer.lixlr.mongodb.net/test?authSource=admin&replicaSet=atlas-shng1u-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
+const databaseName = 'resumer'
 
-MongoClient.connect(connectionURL, {useNewUrlParser:true}, (error,client) => {
-    if(error){
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
         return console.log('Unable to connect to the database')
     }
-    const db=client.db(databaseName)
+    else {
+        console.log('Connected to database')
+    }
+    const db = client.db(databaseName)
 })
